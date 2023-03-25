6f3255251fa25ceae6c1c0117f92852d22d1ee82
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2023-03-25 12:47:54 +0700

    Added Extra Dependencies, ESLint Configuration, File Ignore, And Import Bootstrap To Front End Project

--- talent-growth-full-stack-front-end-react/pages/_app.tsx
+++ talent-growth-full-stack-front-end-react/pages/_app.tsx
@@ -1,5 +1,10 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
   return <Component {...pageProps} />
 }
